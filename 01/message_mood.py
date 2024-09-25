fc29d2720d8d3541a15ac289cb46a71bd6c15a5c
s1lver29 <nikitaselver@yandex.ru>
2024-09-25 22:20:40 +0300

    Added check empty string and file __init__.py

--- 01/message_mood.py
+++ 01/message_mood.py
@@ -10,25 +10,27 @@ def predict_message_mood(
     if not isinstance(message, str):
         raise TypeError(
             f"Получено {type(message).__name__}. "
-            "message должна быть строкой (str)"
+            "message должна быть строкой (str)."
         )
+    if not message.strip():
+        raise ValueError("Ожидалось, что message не пустая строка.")
     if not 0 <= bad_thresholds <= 1:
         raise ValueError(
             f"Получено {bad_thresholds=}. "
-            "Значение bad_thresholds лежит от (0, 1)"
+            "Значение bad_thresholds лежит от (0, 1)."
         )
     if not 0 <= good_thresholds <= 1:
         raise ValueError(
             f"Получено {good_thresholds=}. "
-            "Значение good_thresholds лежит (0, 1)"
+            "Значение good_thresholds лежит (0, 1)."
         )
     if bad_thresholds > good_thresholds:
         raise ValueError(
-            "bad_thresholds должен быть меньше или равен good_thresholds"
+            "bad_thresholds должен быть меньше или равен good_thresholds."
         )
 
     model = SomeModel()
-    pred = model.predict(message=message.lower()) # pylint: disable=E1111
+    pred = model.predict(message=message.lower())  # pylint: disable=E1111
 
     if pred > good_thresholds:
         return "отл"
