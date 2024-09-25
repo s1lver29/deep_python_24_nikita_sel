5675a6aea4d3b9ef07d15dcc0f14c5e90ac9b714
s1lver29 <nikitaselver@yandex.ru>
2024-09-25 23:05:39 +0300

    Refactoring code and type checking forbad_thresholds and good_thresholds

--- 01/message_mood.py
+++ 01/message_mood.py
@@ -1,7 +1,6 @@
 class SomeModel:
-    # pylint: disable=too-few-public-methods
-    def predict(self, message: str) -> float:
-        pass
+    # pylint: disable=too-few-public-methods, unused-argument
+    def predict(self, message: str) -> float: ...
 
 
 def predict_message_mood(
@@ -9,20 +8,31 @@ def predict_message_mood(
 ) -> str:
     if not isinstance(message, str):
         raise TypeError(
-            f"Получено {type(message).__name__}. "
+            f"Получено {type(message).__name__}, "
             "message должна быть строкой (str)."
         )
     if not message.strip():
         raise ValueError("Ожидалось, что message не пустая строка.")
+
+    if not isinstance(bad_thresholds, (int, float)):
+        raise TypeError(
+            f"Получено {type(bad_thresholds).__name__}, "
+            "bad_thresholds должен быть числом"
+        )
+    if not isinstance(good_thresholds, (int, float)):
+        raise TypeError(
+            f"Получено {type(good_thresholds).__name__}, "
+            "good_thresholds должен быть числом."
+        )
     if not 0 <= bad_thresholds <= 1:
         raise ValueError(
-            f"Получено {bad_thresholds=}. "
-            "Значение bad_thresholds лежит от (0, 1)."
+            f"Получено {bad_thresholds=}, "
+            "значение bad_thresholds лежит от (0, 1)."
         )
     if not 0 <= good_thresholds <= 1:
         raise ValueError(
-            f"Получено {good_thresholds=}. "
-            "Значение good_thresholds лежит (0, 1)."
+            f"Получено {good_thresholds=}, "
+            "значение good_thresholds лежит (0, 1)."
         )
     if bad_thresholds > good_thresholds:
         raise ValueError(
