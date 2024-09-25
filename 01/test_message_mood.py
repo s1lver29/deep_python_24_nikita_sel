1ea2c22a1dcd2161ee98f799d0467f581309069d
s1lver29 <nikitaselver@yandex.ru>
2024-09-25 22:27:42 +0300

    Added tests for message_mood.py

---
+++ 01/test_message_mood.py
@@ -0,0 +1,106 @@
+import unittest
+from unittest.mock import patch
+
+from .message_mood import predict_message_mood, SomeModel
+
+
+class TestPredMessageMood(unittest.TestCase):
+    def setUp(self) -> None:
+        self.valid_message = "message"  # Валидное значение
+        print(f"\nStart test {self.id()}")
+
+    def tearDown(self) -> None:
+        print(f"End test {self.id()}")
+
+    @patch.object(SomeModel, "predict")
+    def test_good_message(self, mock_predict):
+        """
+        good_threshold дефолтное значение
+        Проверка, если сообщение больше good_threshold
+        """
+        mock_predict.return_value = 0.9
+
+        pred = predict_message_mood("Чапаев и пустота")
+        self.assertEqual(pred, "отл")
+
+    @patch.object(SomeModel, "predict")
+    def test_bad_message(self, mock_predict):
+        """
+        bad_threshold дефолтное значение
+        Проверка, если сообщения меньше bad_threshold
+        """
+        mock_predict.return_value = 0.1
+
+        pred = predict_message_mood("Вулкан")
+        self.assertEqual(pred, "неуд")
+
+    @patch.object(SomeModel, "predict")
+    def test_normal_message(self, mock_predict):
+        """
+        [bad_threshold; good_threshold] дефолтные значения
+        Проверка, если значение в районе [bad_threshold; good_threshold]
+        """
+        mock_predict.return_value = 0.5
+
+        pred = predict_message_mood("Нормально")
+        self.assertEqual(pred, "норм")
+
+    def test_empty_message(self):
+        """
+        Проверка, если сообщение пустое, то возвратом будет ошибка,
+        что строка пустая
+        """
+        with self.assertRaises(ValueError):
+            predict_message_mood("")
+
+    def test_incorrect_thresholds(self):
+        """
+        Проверка, что bad_thresholds и good_thresholds некорректные
+        """
+        with self.assertRaises(ValueError):
+            predict_message_mood(self.valid_message, bad_thresholds=-1)
+        with self.assertRaises(ValueError):
+            predict_message_mood(self.valid_message, bad_thresholds=2)
+        with self.assertRaises(ValueError):
+            predict_message_mood(self.valid_message, good_thresholds=-1)
+        with self.assertRaises(ValueError):
+            predict_message_mood(self.valid_message, good_thresholds=2)
+        with self.assertRaises(ValueError):
+            predict_message_mood(
+                self.valid_message, bad_thresholds=1, good_thresholds=0.5
+            )
+
+    def test_message_is_not_string(self):
+        """
+        Проверка, что message не строка
+        """
+        with self.assertRaises(TypeError):
+            predict_message_mood(message=123)
+        # with self.assertRaises(TypeError):
+        # predict_message_mood(message=None)
+
+    @patch.object(SomeModel, "predict")
+    def test_valid_equal_thresholds(self, mock_predict):
+        """
+        Проверка, что при равенстве bad_thresholds и good_thresholds
+        отрабатывать будет корректно
+        """
+        test_cases = [
+            (0.5, 0.5, 0.49, "неуд"),
+            (0.5, 0.5, 0.5, "норм"),
+            (0.5, 0.5, 0.51, "отл"),
+        ]
+        for (
+            bad_thresholds,
+            good_thresholds,
+            model_prediction,
+            expected,
+        ) in test_cases:
+            mock_predict.return_value = model_prediction
+
+            result = predict_message_mood(
+                "Тестовое сообщение",
+                bad_thresholds=bad_thresholds,
+                good_thresholds=good_thresholds,
+            )
+            self.assertEqual(result, expected)
