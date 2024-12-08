7f4edbf3529566ef22d4c8e8a5e67d54513aa101
s1lver29 <nikitaselver@yandex.ru>
2024-12-09 01:38:11 +0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,5 @@ folders=("01" "02" "03" "04" "05" "06" "07")
 for folder in "${folders[@]}"; do
   echo "Running tests for $folder/test_*.py"
   coverage run -m unittest discover -s "$folder" -p "test_*.py" -t "."
+  coverage report -m
 done
