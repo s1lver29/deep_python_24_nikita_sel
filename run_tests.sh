3bdf4464c27175ff093681f9f56485d29923f144
s1lver29 <nikitaselver@yandex.ru>
2024-12-09 01:59:26 +0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,9 @@ folders=("01" "02" "03" "04" "05" "06" "07")
 for folder in "${folders[@]}"; do
   echo "Running tests for $folder/test_*.py"
   coverage run -m unittest discover -s "$folder" -p "test_*.py" -t "."
+  if [ $? -ne 0 ]; then
+    echo "Tests failed in folder $folder"
+    exit 1
+  fi
   coverage report -m
 done
